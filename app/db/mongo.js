2f1be9384b15fd6e86f6ecd5d0c7183b2549f356
Gabriel Prates <gabsprates@gmail.com>
2017-07-30 22:22:13 -0300

    Melhorando as configurações com variáveis de ambiente

--- app/db/mongo.js
+++ app/db/mongo.js
@@ -1,13 +1,12 @@
-const config = require('config');
 const mongoose = require('mongoose');
 
 function __connection() {
 
-  let username  = config.get('mongo.username');
-  let password  = config.get('mongo.password');
-  let database  = config.get('mongo.database');
-  let server    = config.get('mongo.server');
-  let port      = config.get('mongo.port');
+  let username  = process.env.MONGO_DATABASE || '';
+  let password  = process.env.MONGO_USERNAME || '';
+  let database  = process.env.MONGO_PASSWORD || 'sapucaia';
+  let server    = process.env.MONGO_SERVER || 'localhost';
+  let port      = process.env.MONGO_PORT || 27017;
   let auth      = username ? (username + ':' + password + '@') : '';
 
   return 'mongodb://' + auth + server + ':' + port + '/' + database;
