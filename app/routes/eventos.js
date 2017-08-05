5f3b03b09b86218ac13b5cb2133b5130fa001f26
Gabriel Prates <gabsprates@gmail.com>
2017-08-05 00:28:57 -0300

    Desacoplando e padronizando

--- app/routes/eventos.js
+++ app/routes/eventos.js
@@ -1,40 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Evento  = require('../models/evento');
-const moment  = require('moment');
-const jwt     = require('jwt-simple');
-
-const middlewareAuth = function(req, res, next) {
-
-  const lancaErro = function(msg, code) {
-    let err = new Error(msg);
-    err.status = code;
-    return err;
-  }
-
-  let token = req.body.token || req.headers['x-access-token'];
-
-  try {
-
-    if (!token) {
-      throw lancaErro("Proibido", 403);
-    }
-
-    let decoded = jwt.decode(token, process.env.JWT_SECRET);
-    let expirou = moment(decoded.expira).isBefore(new Date());
-
-    if (expirou) {
-      throw lancaErro("Acesso não autorizado", 401);
-    }
-
-    req.user = decoded.username;
-    next();
-
-  } catch (e) {
-    return next(e);
-  }
-
-};
+const middlewareAuth = require('../utils/middlewareAuth');
 
 router.route('/')
   .post(middlewareAuth, function(req, res) {
