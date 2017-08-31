a92638798d253b8ca5de490189222ebeb262c013
Gabriel Prates <gabsprates@gmail.com>
2017-08-31 20:19:34 -0300

    organizando um pouco e adaptando GET de eventos para filtros futuros

--- app/routes/eventos.js
+++ app/routes/eventos.js
@@ -2,61 +2,73 @@ const express = require('express');
 const router  = express.Router();
 const Evento  = require('../models/evento');
 const middlewareAuth = require('../utils/middlewareAuth');
-const moment = require('moment')
+const moment = require('moment');
 
-router.route('/')
-  .post(middlewareAuth, function(req, res) {
-    var evento = new Evento(req.body);
-    evento.save(function(error) {
-      if (error) {
-        res.send(error);
-      }
-      res.json(
-        {
-          message: 'Event created',
-          event: evento
-        }
-      );
+router.post('/', middlewareAuth, function(req, res) {
+  let evento = new Evento(req.body);
+  evento.save(function(err) {
+
+    if (err) {
+      res.send(err);
+    }
+    res.json({
+      message: 'Event created',
+      event: evento
     });
-  })
-  .get(function(req, res) {
-    Evento.find({'data.inicio' : { $gte : moment().startOf('day').format()} }, function(err, eventos) {
+
+  });
+});
+
+router.get('/', function(req, res) {
+  let filtro = {};
+
+  filtro['data.inicio'] = {
+    $gte: moment().startOf('day').format()
+  };
+
+  Evento
+    .find(filtro, function(err, eventos) {
       if (err) {
         res.send(err);
       }
       res.json(eventos);
-    });
-  });
+    })
+    .sort('data.inicio')
+  ;
+});
 
-router.route('/:id')
-  .get(function(req, res) {
-    Evento.findById(req.params.id, function(error, evento) {
-      if (error) {
-        res.send(error);
-      }
-      res.json(evento);
-    });
-  })
-  .put(middlewareAuth, function(req, res) {
-    Evento.findOneAndUpdate({_id: req.params.id}, req.body,  {new: true},function(error, evento){
-      if (error) {
-        res.send(error);
-      }
-      res.send(
-        {
-          message:"Event updated",
-          event: evento
-        }
-      );
-    });
-  })
-  .delete(middlewareAuth, function(req, res) {
-    Evento.remove({_id: req.params.id}, function(error) {
-      if (error) {
-        res.send(error);
-      }
-      res.json({ message: 'Event removed'});
-    });
+router.get('/:id', function(req, res) {
+  Evento.findById(req.params.id, function(err, evento) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(evento);
   });
+});
+
+router.put('/:id', middlewareAuth, function(req, res) {
+  Evento.findOneAndUpdate({
+    _id: req.params.id
+  }, req.body, { new: true }, function(err, evento) {
+
+    if (err) {
+      res.send(err);
+    }
+    res.send({
+      message:"Event updated",
+      event: evento
+    });
+
+  });
+});
+
+router.delete('/:id', middlewareAuth, function(req, res) {
+  Evento.remove({ _id: req.params.id }, function(err) {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: 'Event removed' });
+  });
+});
 
 module.exports = router;
