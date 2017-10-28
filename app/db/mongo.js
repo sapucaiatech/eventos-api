60d2af679435adc70e28889c5f79567121a08625
Gabriel Prates <gabsprates@gmail.com>
2017-10-28 09:58:22 -0200

    consertando a atribuição das variáveis

--- app/db/mongo.js
+++ app/db/mongo.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 function __connection() {
 
-  let username  = process.env.MONGO_DATABASE || '';
-  let password  = process.env.MONGO_USERNAME || '';
-  let database  = process.env.MONGO_PASSWORD || 'sapucaia';
+  let username  = process.env.MONGO_USERNAME || '';
+  let password  = process.env.MONGO_PASSWORD || '';
+  let database  = process.env.MONGO_DATABASE || 'sapucaia';
   let server    = process.env.MONGO_SERVER || 'localhost';
   let port      = process.env.MONGO_PORT || 27017;
   let auth      = username ? (username + ':' + password + '@') : '';
