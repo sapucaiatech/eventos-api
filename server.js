700dbe05506c5d29ba1ac67c0289197451f7345b
danielmadu <daniel.madureira.batista@gmail.com>
2017-10-30 10:55:55 -0200

    adicionado o morgan para log de acesso

--- server.js
+++ server.js
@@ -2,21 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const logger = require('morgan');
 
 const port = process.env.PORT || 8000;
+const dev = process.env.DEV || true;
 
 // Config
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+if (dev){
+  app.use(logger('combined'));
+}
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
-  let date = new Date();
-  console.log(`\n -- ${req.method} ::> ${req.originalUrl}`);
-  console.log(`request at: ${date.toString()}`);
   next();
 });
 
